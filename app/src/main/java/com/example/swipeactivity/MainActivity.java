3598e85491411cf6c99cda94aea75d249c3a7e24
dhirajsawant98@gmail.com <Dhiraj@1998>
2021-03-28 13:54:08 +0530

    Swipe

---
+++ app/src/main/java/com/example/swipeactivity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.swipeactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureDetectorCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gestureDetectorCompat = new GestureDetectorCompat(this, new GestureClass());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class GestureClass extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+            if (e2.getX() < e1.getX()) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            return true;
+        }
+    }
+}
